a61f5f22bc8a23ae2bbed026f86a0c166f448c85
Travis Frazier <Travis.Frazier@ivanti.com>
2023-01-04 09:38:20 -0800

    added greensock

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+gsap.set('img', {
+    opacity: 0
+})
+
+gsap.to('.image-v1', {
+    opacity: 1,
+    duration: 3,
+    repeat: 0
+})
+
+gsap.to('.image-v2', {
+    opacity: 0.4
+})
+
+gsap.set('.feature-section-item', {
+    opacity: 0,
+})
+
+gsap.to('.feature-section-item', {
+    opacity: 1,
+    duration: 3,
+    scrollTrigger: {
+        trigger: '.feature-section'
+    },
+    stagger: 0.5
+})
